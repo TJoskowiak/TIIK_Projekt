9b1df7e2ff6c830a0f50592c8d994e283647f0de
TJoskowiak <tomaszjoskowiak@gmail.com>
2020-01-03 17:43:55 +0100

    Added Crypto class. Tweaking needed

--- src/UI/LoadFileButton.java
+++ src/UI/LoadFileButton.java
@@ -1,10 +1,13 @@
 package UI;
 
+import Crypto.LZ77;
+
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.IOException;
 
 public class LoadFileButton extends JButton implements ActionListener {
 
@@ -24,6 +27,13 @@ public class LoadFileButton extends JButton implements ActionListener {
         if (returnValue == JFileChooser.APPROVE_OPTION)
         {
             StatisticsTable.analyzedFile = fileChooser.getSelectedFile();
+            LZ77 lz = new LZ77();
+            try {
+                lz.compress(fileChooser.getSelectedFile().toString());
+                lz.unCompress(fileChooser.getSelectedFile().toString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
